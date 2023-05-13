e3807bd76701432ec437e6cec824f095dcf273fb
CYNTHIA TRAN <yencynthia.tran@gmail.com>
2023-05-12 21:34:13 -0400

    css NavBar

--- frontend/src/shared/components/NavBar.js
+++ frontend/src/shared/components/NavBar.js
@@ -8,12 +8,14 @@ function NavBar() {
     return (
       <div className="nav">
             <a href="https://www.cmontmorency.qc.ca/"><img src={logo} alt={logo}></img></a>
+            <div className="nav-menu">
             <Link to="/">Page d'accueil</Link>
             <Link to="/PagesInformatives">Pages Informatives</Link>
             <Link to="/Stages/AjouterStage">Ajouter Stage</Link>
             <Link to="/Stages">Stages Disponibles</Link>
             <Link to="/Etudiants/AjouterEtudiant">Ajouter Etudiant</Link>
             <Link to="/Etudiants">Étudiants</Link>
+            </div>
       </div>
     );
   }
