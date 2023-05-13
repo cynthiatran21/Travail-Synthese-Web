e83077bc2c8d017ac6a9083cfbf0089a35a3668e
ARIANE LEWIS <lewis.ariane@gmail.com>
2023-05-12 20:21:02 -0400

    Afficher etudiants fonctionne

--- frontend/src/etudiants/components/EtudiantList.js
+++ frontend/src/etudiants/components/EtudiantList.js
@@ -4,9 +4,9 @@ import Card from '../../shared/components/UIElements/Card';
 import EtudiantItem from './EtudiantItem';
 import Button from '../../shared/components/FormElements/Button';
 
-const EtudiantList = props => {
-  console.log(props);
-  if (props.items.length === 0) {
+function EtudiantList ({etudiantsDispo, longueur}) {
+
+  if (longueur === 0) {
     return (
       <div>
         <Card>
@@ -17,16 +17,20 @@ const EtudiantList = props => {
     );
   }
 
+  console.log("essai")
+  console.log(etudiantsDispo);
   return (
     <ul>
-      {props.items.map(etu => (
-        <EtudiantItem
-          key={etu.id}
-          noDA={etu.noDA}
-          nomEtudiant={etu.nomEtudiant}
-          courrielEtudiant={etu.courrielEtudiant}
-          profilSortie={etu.profilSortie}
+      {etudiantsDispo.map((etu, index) => (
+        <Card key={index}>
+          <EtudiantItem
+            key={etu.id}
+            noDA={etu.noDA}
+            nomEtudiant={etu.nomEtudiant}
+            courrielEtudiant={etu.courrielEtudiant}
+            profilSortie={etu.profilSortie}
         />
+        </Card>
       ))}
     </ul>
   );
--- frontend/src/etudiants/pages/Etudiants.js
+++ frontend/src/etudiants/pages/Etudiants.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import EtudiantList from "../components/EtudiantList";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
-function Etudiants(){
+
+const EtudiantsDispo = () => {
+  const {error, sendRequest, clearError } = useHttpClient();
+  const [etudiantsDispo, setEtudiantsDispo] = useState([]);
+  const [tabTemp, setTabTemp] = useState([]);
+  const [longueur, setLongueur] = useState([]);
+
+  useEffect(() => {
+    const recupererEtudiants = async () => {
+      try{
+
+        
+        const reponseData = await sendRequest("http://localhost:5000/api/etudiants")
+
+        let longueur = reponseData.etudiants.length;
+        
+
+        for (let i = 0; i < longueur; i++) {
+          tabTemp.push(reponseData.etudiants[i]);
+        }
+
+        setEtudiantsDispo(tabTemp);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    recupererEtudiants();
+  }, [sendRequest]);
+
+
+  
+  console.log("EtudiantsDispo " + etudiantsDispo);
+
     return (
-          <div>
-            <h1>Voici la liste des étudiants qui souhaitent appliquer à un stage.</h1>
-            <EtudiantList />
-          </div>
+      <div>
+        <h1>Voici la liste des étudiants</h1>
+           <EtudiantList etudiantsDispo={tabTemp} longueur={longueur} />
+      </div>
       );
-}
+    };
 
-export default Etudiants;
+export default EtudiantsDispo;
