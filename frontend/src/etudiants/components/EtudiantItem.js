c9d5dbd1049050f4748c1fc4ee66b628e20a2617
CYNTHIA TRAN <yencynthia.tran@gmail.com>
2023-05-16 17:55:56 -0400

    essaie assigner etudiant a stage

--- frontend/src/etudiants/components/EtudiantItem.js
+++ frontend/src/etudiants/components/EtudiantItem.js
@@ -1,23 +1,21 @@
-import React, { useState, useEffect} from 'react';
-import { useForm } from '../../shared/hooks/form-hook';
-import { useHttpClient } from '../../shared/hooks/http-hook';
-import Button from '../../shared/components/FormElements/Button';
-import Modal from '../../shared/components/UIElements/Modal';
+import React, { useState, useEffect } from "react";
+import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 
-import StagesDispo from '../../stages/pages/StagesDispo';
+import StagesDispo from "../../stages/pages/StagesDispo";
 
-import "../../styles/Etudiants.css"
+import "../../styles/Etudiants.css";
 
-const EtudiantItem = props => {
+const EtudiantItem = (props) => {
   const { error, sendRequest, clearError } = useHttpClient();
 
-  const [assignationFonctionne, setAssignationFonctionne] = useState(true)
-  const [show, setShow] = useState(false)
+  const [assignationFonctionne, setAssignationFonctionne] = useState(true);
+  const [show, setShow] = useState(false);
 
-  
   const [stagesDispo, setStagesDispo] = useState([]);
   const [longueur, setLongueur] = useState(0);
-
 
   const [stagesDispoProg, setStagesDispoProg] = useState([]);
   const [stagesDispoReseau, setStagesDispoReseau] = useState([]);
@@ -27,44 +25,42 @@ const EtudiantItem = props => {
 
   const [formState, inputHandler] = useForm(
     {
-      satge: {
-          value: '',
-          isValid: true
-      }
+      stage: {
+        value: "",
+        isValid: true,
+      },
     },
     true
   );
 
+  const x = document.querySelector("#stage");
 
-
-
-
-
-
-  
   useEffect(() => {
     const recupererStages = async () => {
-      
-      try{
-        let tabTemp=[];
+      try {
+        let tabTemp = [];
 
-        if(props.profilSortie === "Réseaux et sécurité"){
-          const reponseData = await sendRequest("http://localhost:5000/api/stages/Réseaux et sécurité")
-        
+        if (props.profilSortie === "Réseaux et sécurité") {
+          const reponseData = await sendRequest(
+            "http://localhost:5000/api/stages/Réseaux et sécurité"
+          );
+
           let long = reponseData.stages.length;
-  
+
           setLongueurReseau(long);
           setLongueur(longueurReseau);
-  
+
           for (let i = 0; i < longueurReseau; i++) {
             tabTemp.push(reponseData.stages[i]);
           }
-          
+
           setStagesDispoReseau(tabTemp);
           setStagesDispo(stagesDispoReseau);
-        } else if(props.profilSortie === "Développement d'application"){
-          const reponseData = await sendRequest("http://localhost:5000/api/stages/Développement d'applications")
-        
+        } else if (props.profilSortie === "Développement d'application") {
+          const reponseData = await sendRequest(
+            "http://localhost:5000/api/stages/Développement d'applications"
+          );
+
           let long = reponseData.stages.length;
 
           setLongueurProg(long);
@@ -73,11 +69,10 @@ const EtudiantItem = props => {
           for (let i = 0; i < longueurProg; i++) {
             tabTemp.push(reponseData.stages[i]);
           }
-        
+
           setStagesDispoProg(tabTemp);
           setStagesDispo(stagesDispoProg);
         }
-        
       } catch (err) {
         console.log(err);
       }
@@ -85,77 +80,67 @@ const EtudiantItem = props => {
     recupererStages();
   }, [sendRequest, stagesDispo, longueur]);
 
+  
 
-console.log("TEST_______________________________________________________-")
-console.log(stagesDispo);
-console.log("TEST_______________________________________________________-")
+  // var x = document.getElementById("stage");
+  // var option = document.createElement("option");
 
+  // for (let i = 0; i < longueur; i++) {
+  //   var option = document.createElement("option");
+  //   option.text = stagesDispo[i].nomEntreprise;
+  //   x.add(option, x[i]);
+  // }
 
-// var x = document.getElementById("stage");
-// var option = document.createElement("option");
+  function afficherOptions() {
+    setShow(true);
+    console.log("TEST_______________________________________________________-");
+    console.log(document.querySelector("#buttonStage"))
+  console.log(stagesDispo);
+  console.log("TEST_______________________________________________________-");
+    
+    for (let i = 0; i < longueur; i++) {
+      var option = document.createElement("option");
+      option.text = stagesDispo[i].nomEntreprise;
+      console.log(option)
+      console.log(x)
+      //x.add(option, x[i]);
+   
+        $('#stage').append(`<option value="${option}">${option}</option>`);
+      
+    }
+  }
 
-
-// for (let i = 0; i < longueur; i++) {
-//   var option = document.createElement("option");
-//   option.text = stagesDispo[i].nomEntreprise;
-//   x.add(option, x[i]);
-// }
-
-
-function afficherOptions(){
-
-};
-
-
-  const choixStageHandler  = async event =>  {
+  const choixStageHandler = async (event) => {
     //test
   };
 
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
   return (
     <React.Fragment>
-      <div onClick={()=> setShow(true)} className="etudiantItem">
-        <h1 >{props.nomEtudiant}</h1>
+      <div onClick={() => afficherOptions()} className="etudiantItem">
+        <h1>{props.nomEtudiant}</h1>
         <p>Numéro de DA: {props.noDA}</p>
         <p>Courriel: {props.courrielEtudiant}</p>
         <p>Profil de sortie: {props.profilSortie}</p>
       </div>
 
-      
-      <Modal title="Assigner un étudiant à un stage" onClose={() => setShow(false)} show={show}>
+      <Modal
+        title="Assigner un étudiant à un stage"
+        onClose={() => setShow(false)}
+        show={show}
+      >
         <p>Choisir un stage</p>
         <form onSubmit={choixStageHandler}>
-          <label>Profil de sortie de l'étudiant: </label>
-          <select
-            id="stage"
-          >
-            <option value="Réseaux et sécurité">Réseaux et sécurité</option>
-            <option value="Développement d'applications">Développement d'applications</option>
+          <label>Stages disponibles: </label>
+          <select id="stage">
           </select>
 
-          <StagesDispo />
-
           <br></br>
-          <Button type="submit" disabled={!formState.isValid}>
+          <Button id="buttonStage" type="submit" disabled={!formState.isValid}>
             Assigner le stage à l'étudiant
           </Button>
         </form>
       </Modal>
+      
     </React.Fragment>
   );
 };
