8c80d25a4cd13300a071cde22ca4123b4319d119
ARIANE LEWIS <lewis.ariane@gmail.com>
2023-05-17 11:36:56 -0400

    Continu essai

--- frontend/src/etudiants/components/EtudiantItem.js
+++ frontend/src/etudiants/components/EtudiantItem.js
@@ -4,8 +4,6 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
 
-import StagesDispo from "../../stages/pages/StagesDispo";
-
 import "../../styles/Etudiants.css";
 
 const EtudiantItem = (props) => {
@@ -32,7 +30,7 @@ const EtudiantItem = (props) => {
     },
     true
   );
-  
+
   const [showOnce, setShowOnce] = useState(0);
   const selectRef = useRef();
 
@@ -52,12 +50,17 @@ const EtudiantItem = (props) => {
           setLongueur(longueurReseau);
 
           for (let i = 0; i < longueurReseau; i++) {
-            tabTemp.push(reponseData.stages[i]);
+            if (
+              reponseData.stages[i].stagiaires.length <
+              reponseData.stages[i].nbPostesDispo
+            ) {
+              tabTemp.push(reponseData.stages[i]);
+            }
           }
 
           setStagesDispoReseau(tabTemp);
           setStagesDispo(stagesDispoReseau);
-        } else if (props.profilSortie === "Développement d'application") {
+        } else if (props.profilSortie === "Développement d'applications") {
           const reponseData = await sendRequest(
             "http://localhost:5000/api/stages/Développement d'application"
           );
@@ -68,7 +71,12 @@ const EtudiantItem = (props) => {
           setLongueur(longueurProg);
 
           for (let i = 0; i < longueurProg; i++) {
-            tabTemp.push(reponseData.stages[i]);
+            if (
+              reponseData.stages[i].stagiaires.length <
+              reponseData.stages[i].nbPostesDispo
+            ) {
+              tabTemp.push(reponseData.stages[i]);
+            }
           }
 
           setStagesDispoProg(tabTemp);
@@ -81,67 +89,73 @@ const EtudiantItem = (props) => {
     recupererStages();
   }, [sendRequest, stagesDispo, longueur]);
 
-  
-
-  // var x = document.getElementById("stage");
-  // var option = document.createElement("option");
-
-  // for (let i = 0; i < longueur; i++) {
-  //   var option = document.createElement("option");
-  //   option.text = stagesDispo[i].nomEntreprise;
-  //   x.add(option, x[i]);
-  // }
-
   useEffect(() => {
-    // Access the select element after the component has rendered
-    if(show && showOnce < 1){
-    const select = selectRef.current;
-    if (select) {
-      console.log(longueur)
-      console.log(showOnce)
-      // Manipulate the select element here
-      for (let i = 0; i < longueur; i++) {
-        var option = document.createElement("option");
-        option.text = stagesDispo[i].nomEntreprise;
-        console.log("TEST_____________________________________")
-        console.log(option)
-        console.log(select)
-        console.log("TEST______________________________________")
-        select.add(option, undefined);
+    if (show && showOnce < 1) {
+      const select = selectRef.current;
+      if (select) {
+        console.log(longueur);
+        console.log(showOnce);
+        for (let i = 0; i < longueur; i++) {
+          var option = document.createElement("option");
+          option.text = stagesDispo[i].nomEntreprise;
+          //option.value = stagesDispo[i];
+          //console.log(option.value)
+          console.log("TEST_____________________________________");
+          console.log(option);
+          console.log(select);
+          console.log("TEST______________________________________");
+          select.add(option, undefined);
+        }
+        setShowOnce(showOnce + 1);
       }
-      setShowOnce(showOnce+1)
     }
-  }
   }, [show, longueur, stagesDispo]);
 
   useEffect(() => {
-    // Access the select element after the component has rendered
-    if(!show){
-    setShowOnce(0);
-  }
-  }, [show, longueur, stagesDispo]);
-  
-  /*function afficherOptions() {
-    setShow(true);
-    console.log("TEST_______________________________________________________-");
-    console.log(selectRef.current)
-  console.log(stagesDispo);
-  console.log("TEST_______________________________________________________-");
-    
-    for (let i = 0; i < longueur; i++) {
-      var option = document.createElement("option");
-      option.text = stagesDispo[i].nomEntreprise;
-      console.log(option)
-      //console.log(x)
-      //x.add(option, x[i]);
-   
-        //x.append(`<option value="${option}">${option}</option>`);
-        selectRef.current.add(option,undefined);
+    if (!show) {
+      setShowOnce(0);
     }
-  }*/
+  }, [show, longueur, stagesDispo]);
 
+
+
+  
   const choixStageHandler = async (event) => {
-    //test
+    //assigner le stage a letudiant : ajouter l'etudiant a la liste de stagiaires, ajouter le stage a l'etudiant
+
+    event.preventDefault();
+    console.log(formState.inputs); // send this to the backend!
+
+    console.log("BLABLA__________________________________")
+    console.log(document.querySelector("#stage").value)
+
+    //formState.inputs.profilSortie.value = document.querySelector("#profilSortie").value;
+
+    /*try {
+
+      //router.patch("/:etudiantId", controleursEtudiant.assignerStage);
+      const reponseData = await sendRequest(
+        "http://localhost:5000/api/etudiants",
+        "PATCH",
+        JSON.stringify({
+          nomContact: formState.inputs.noDA.value,
+          courrielContact: formState.inputs.nomEtudiant.value,
+          nomEntreprise: formState.inputs.courrielEtudiant.value,
+          adresseEntreprise: document.querySelector("#profilSortie").value,
+          typeStage: formState.inputs.courrielEtudiant.value,
+          nbPostesDispo: formState.inputs.courrielEtudiant.value,
+          description: formState.inputs.courrielEtudiant.value,
+        }),
+        {
+          "Content-Type": "application/json",
+        }
+      );
+      //setAjoutFonctionne(true);
+      console.log(reponseData);
+    } catch (err) {
+      //setAjoutFonctionne(false);
+      console.log(err);
+    }*/
   };
 
   return (
@@ -161,8 +175,7 @@ const EtudiantItem = (props) => {
         <p>Choisir un stage</p>
         <form onSubmit={choixStageHandler}>
           <label>Stages disponibles: </label>
-          <select ref={selectRef} id="stage">
-          </select>
+          <select ref={selectRef} id="stage"></select>
 
           <br></br>
           <Button id="buttonStage" type="submit" disabled={!formState.isValid}>
@@ -170,7 +183,6 @@ const EtudiantItem = (props) => {
           </Button>
         </form>
       </Modal>
-      
     </React.Fragment>
   );
 };
