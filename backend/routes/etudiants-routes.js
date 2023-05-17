d339940089ae91d821f274a49b5d815bd6934591
CYNTHIA TRAN <yencynthia.tran@gmail.com>
2023-05-17 15:53:06 -0400

    modif route patch etudiant

--- backend/routes/etudiants-routes.js
+++ backend/routes/etudiants-routes.js
@@ -3,7 +3,7 @@ const controleursEtudiant = require("../controllers/etudiants-controleurs");
 const router = express.Router();
 
 router.post("/", controleursEtudiant.ajouterEtudiant);
-router.patch("/:etudiantId", controleursEtudiant.assignerStage);
+router.patch("/", controleursEtudiant.assignerStage);
 router.get("/", controleursEtudiant.getEtudiants);
 
 module.exports = router;
